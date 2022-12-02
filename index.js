3860d40c781840294f90f79b21f45259f985d735
MatthiasVandersanden <matthias.vandersanden@designexpress.eu>
2022-12-02 16:44:06 +0100

    Update

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ function getOctokitSingleton() {
   const token = core.getInput('token');
   core.debug(`Token: ${token}`);
   octokitSingleton = github.getOctokit(token);
+  core.debug(`Octokit: ${octokitSingleton}`);
   return octokitSingleton;
 }
 
